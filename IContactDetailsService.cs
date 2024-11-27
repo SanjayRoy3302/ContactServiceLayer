2258d6b5b017bffff1127b18221cd2d79d6c4759
sanjay <cs@DESKTOP-Q2Q54VN>
2024-11-27 23:14:25 +0530

    Service layer added to repository

---
+++ IContactDetailsService.cs
@@ -0,0 +1,12 @@
+﻿using ContactServiceLayer.Models;
+
+namespace ContactServiceLayer
+{
+    public interface IContactDetailsService
+    {
+        List<ContactDetails> GetAllContactsList();
+        BaseResponseModel AddContact(ContactDetails contactDetails);
+        BaseResponseModel UpdateContact(ContactDetails contactDetails);
+        BaseResponseModel DeleteContact(int contactDetailId);
+    }
+}
---
+++ Models/BaseResponseModel.cs
@@ -0,0 +1,29 @@
+﻿namespace ContactServiceLayer.Models
+{
+    public class BaseResponseModel
+    {
+        public int Id { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; } = string.Empty;
+    }
+
+    public enum StatusCode
+    {
+        Failed = 0,
+        Added = 1,
+        Updated = 2,
+        Deleted = 3,
+        NotFound = 4,
+        IdMisMatch = 5
+    }
+
+    public class StatusMessage
+    {
+        public const string Added = "Added successfully!";
+        public const string Updated = "Updated successfully!";
+        public const string Deleted = "Deleted successfully!";
+        public const string Failed = "Operation Failed!";
+        public const string NotFound = "Cannot find the given Id in db";
+        public const string IdMisMatch = "Given id doesn't match with id in database";
+    }
+}
